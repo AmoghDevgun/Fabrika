01d5e651b9fdf3c3e3c06f1e3327afbef5c35531
Amogh Devgun <amoghdevgun@gmail.com>
2025-08-17 22:35:01 +0530

    feat 1

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,13 +1,37 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
-      
-        <img src="" alt="" />
+    
+        <img src={assets.logo} className='w-36' alt="Logo" />
 
+        <ul className='flex gap-5 items-center text-sm text-gray-700'>
+
+            <NavLink to='/'>
+                <p>Home</p>
+                <hr className='hidden' />
+            </NavLink>
+
+            <NavLink to='/collection'>
+                <p>Collection</p>
+                <hr className='hidden' />
+            </NavLink>
+            
+            <NavLink to='/about'>
+                <p>About</p>
+                <hr className='hidden' />
+            </NavLink>
+
+            <NavLink to='/contact'>
+                <p>Contact</p>
+                <hr className='hidden' />
+            </NavLink>
+
+        </ul>
+    
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ export { default as Login } from './Login'
 export { default as Orders } from './Orders'
 export { default as PlaceOrder } from './PlaceOrder'
 export { default as Product } from './Product'
-export { default as Login } from './Login'
 
 
 
