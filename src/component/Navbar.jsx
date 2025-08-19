a536364af8f3649a20344f3606097c76202d768c
Amogh Devgun <amoghdevgun@gmail.com>
2025-08-20 04:06:39 +0530

    NAVBAR + HERO

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
-  return (
+  
+    const [visible, setVisible] = useState(false)
+  
+    return (
+
     <div className='flex items-center justify-between py-5 font-medium'>
     
         <img src={assets.logo} className='w-36' alt="Logo" />
 
-        <ul className='flex gap-5 items-center text-sm text-gray-700'>
+        {/* Menu Items */}
+        <ul className='gap-5 items-center text-sm text-gray-700 hidden sm:flex'>
 
             <NavLink to='/'>
                 <p>Home</p>
@@ -31,6 +36,51 @@ function Navbar() {
             </NavLink>
 
         </ul>
+
+        {/* Left Side */}
+        <div className='flex items-center  gap-5 justify-between'>
+            
+            <img src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
+
+            <div className='group relative'>
+                <img src={assets.profile_icon} alt="" className='w-5 cursor-pointer'/>
+                
+                <div className='group-hover:block absolute hidden right-0 pt-4'>
+                    
+                    <div className='flex flex-col bg-slate-100 w-36 p-2.5 rounded gap-1 text-sm '>
+                        <p className='cursor-pointer hover:text-fuchsia-900'>My Profile</p>
+                        <p className='cursor-pointer hover:text-fuchsia-900'>Orders</p>
+                        <p className='cursor-pointer hover:text-fuchsia-900'>Log Out</p>
+                    </div>
+
+                </div>
+            </div>
+
+            <Link to='/cart'>
+                <img src={assets.cart_icon} className='w-5' />
+                
+            </Link>
+
+            <img src={assets.menu_icon} onClick={() => setVisible(true)} className='w-5 sm:hidden cursor-pointer' />
+
+        </div>
+
+        {/* Side Bar for small screens */}
+        <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all  ${visible ? 'w-full': 'w-0'} `}>
+            <div className='flex flex-col text-gray-600'>
+                
+                <div onClick={() => setVisible(false)} className='flex items-center gap-5 p-3 cursor-pointer'>
+                    <img src={assets.dropdown_icon} />
+                    <p>Back</p>
+                </div>
+
+                <NavLink onClick={() => setVisible(false)} to='/' className='py-2 pl-6 border'> Home </NavLink>
+                <NavLink onClick={() => setVisible(false)} to='/collection' className='py-2 pl-6 border'> Collection </NavLink>
+                <NavLink onClick={() => setVisible(false)} to='/about' className='py-2 pl-6 border'> About </NavLink>
+                <NavLink onClick={() => setVisible(false)} to='/contact' className='py-2 pl-6 border'> Contact </NavLink>
+
+            </div>
+        </div>
     
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Hero from '../component/Hero'
 
 function Home() {
   return (
     <div>
-      
+      <Hero />
+
     </div>
   )
 }
